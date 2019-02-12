ca0e9f3d8b98175fe713593754a8a7c05a268174
elfinsanjaya12 <elfinsanjaya1207@gmail.com>
2019-02-12 09:38:16 +0700

    State

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: "Daftar Makanan Nusantara"
+    };
+  }
   render() {
+    const { data } = this.state;
     return (
       <div>
         <h2>Makanan Has Indonesia</h2>
+        <h2>{data}</h2>
       </div>
     );
   }
