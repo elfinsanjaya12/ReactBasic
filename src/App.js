2aa75efb86c072cf287f11f5d0bf3a8358f3bccc
elfinsanjaya12 <elfinsanjaya1207@gmail.com>
2019-02-12 07:04:10 +0700

    Function Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
 import Header from "./components/Header";
 
+const Footer = () => {
+  return (
+    <div>
+      <h2>Ini footer</h2>
+      <p>2019</p>
+    </div>
+  );
+};
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
         <h1>Hello</h1>
+        <Footer />
       </div>
     );
   }
